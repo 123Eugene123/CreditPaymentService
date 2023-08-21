cd135e897a51f3cd76c3c4c04808c7b96cf6a25e
Eugene test <eupushkash@ngs.ru>
2023-08-21 23:08:04 +0300

    Change Commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         int myCredit = service.calculate(1_000_000, 9.99, 12);
         System.out.println(myCredit);
+        myCredit = service.calculate(1_000_000, 9.99, 24);
+        System.out.println(myCredit);
+        myCredit = service.calculate(1_000_000, 9.99, 36);
+        System.out.println(myCredit);
     }
 }
